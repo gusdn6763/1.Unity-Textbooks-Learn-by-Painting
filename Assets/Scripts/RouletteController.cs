045de6a5fafd1042eea9cd207b63342a84774459
Jang <gusdn6763@gmail.com>
2019-11-11 11:40:17 +0900

    2019-11-11
    3강

---
+++ Assets/Scripts/RouletteController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    Collider2D collCheck;
+    Vector2 tapPoint;
+    float rotSpeed;
+
+    private void Awake()
+    {
+        collCheck = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            tapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (collCheck.OverlapPoint(tapPoint))
+                rotSpeed = Random.Range(40f, 50f);
+        }
+
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        this.rotSpeed *= Random.Range(0.94f, 0.96f);
+    }
+}

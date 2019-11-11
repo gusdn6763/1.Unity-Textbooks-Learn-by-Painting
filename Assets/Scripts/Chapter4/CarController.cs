617584864847f88ea52db3de690e2f43727ac2a3
Jang <gusdn6763@gmail.com>
2019-11-11 11:59:47 +0900

    2019-11-11
    4강

---
+++ Assets/Scripts/Chapter4/CarController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private AudioSource audio;
+    float speed = 0;
+    Vector2 startPos;
+    Vector2 endPos;
+
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            startPos = Input.mousePosition;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            endPos = Input.mousePosition;
+            float swipeLength = endPos.x - startPos.x;
+
+            this.speed = swipeLength / 500.0f;
+
+            if (!audio.isPlaying)
+            {
+                audio.Play();
+            }
+
+            else
+            {
+                audio.Stop();
+                audio.Play();
+            }
+        }
+
+        transform.Translate(this.speed, 0, 0);
+        this.speed *= 0.98f;
+    }
+}
---
+++ Assets/Scripts/Chapter4/GameDirector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;  // UI 부품을 사용하기 위해서 중요!
+
+public class GameDirector : MonoBehaviour
+{
+
+    public GameObject car;
+    public GameObject flag;
+    public Text distance;
+    float length;
+
+    void Update()
+    {
+        length = flag.transform.position.x - car.transform.position.x;
+        if (length >= 0) distance.text = "도착 지점까지 " + length.ToString("F2") + "m";
+        else distance.text = "끝";
+    }
+}

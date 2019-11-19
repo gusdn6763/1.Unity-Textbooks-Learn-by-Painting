65bc334f9fd1d4703523e5ca25e1171b581617ef
Jang <gusdn6763@gmail.com>
2019-11-19 23:19:30 +0900

    save

---
+++ Assets/Scripts/chapter3/RouletteController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    Collider2D checkObject;
+    float rotSpeed = 0;  // 회전속도
+    Vector2 tapPoint;
+
+    private void Awake()
+    {
+        checkObject = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        //클릭시        Input.GetMouseButtonDown(0)
+        //클릭 중       Input.GetMouseButton(0);
+        //클릭 뛴 후    Input.GetMouseButtonUp(0);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            tapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if(checkObject.OverlapPoint(tapPoint) == true)
+            {
+                this.rotSpeed = Random.Range(40, 50);
+            }
+
+        }         
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        this.rotSpeed *= Random.Range(0.93f, 0.96f);
+    }
+}
---
+++ Assets/Scripts/chapter8/BasketController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketController : MonoBehaviour
+{
+
+    public AudioClip appleSE;
+    public AudioClip bombSE;
+    AudioSource aud;
+    GameObject director;
+
+    void Awake()
+    {
+        director = GameObject.Find("GameDirector");
+        aud = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Apple")
+        {
+            director.GetComponent<GameDirector>().GetApple();
+            aud.PlayOneShot(this.appleSE);
+        }
+        else
+        {
+            director.GetComponent<GameDirector>().GetBomb();
+            aud.PlayOneShot(this.bombSE);
+        }
+        Destroy(other.gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                float x = Mathf.RoundToInt(hit.point.x);
+                float z = Mathf.RoundToInt(hit.point.z);
+                transform.position = new Vector3(x, 0, z);
+            }
+        }
+    }
+}

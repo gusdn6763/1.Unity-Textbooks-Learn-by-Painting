d4edf6a9221aa2ad38557b6c3b18c8c3198b4026
Jang <gusdn6763@gmail.com>
2019-11-20 09:22:38 +0900

    save_Ac

---
+++ Assets/Scripts/Chapter4/CarController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private AudioSource CarAudio;
+    float speed = 0;
+    Vector2 startPos;
+    Vector2 endPos;
+
+    private void Awake()
+    {
+        CarAudio = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            startPos = Input.mousePosition;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            endPos = Input.mousePosition;
+            float swipeLength = endPos.x - startPos.x;
+
+            this.speed = swipeLength / 500.0f;
+
+            if (!CarAudio.isPlaying)
+            {
+                CarAudio.Play();
+            }
+
+            else
+            {
+                CarAudio.Stop();
+                CarAudio.Play();
+            }
+        }
+
+        transform.Translate(this.speed, 0, 0);
+        this.speed *= 0.98f;
+    }
+}
---
+++ Assets/Scripts/Chapter5/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    void Update()
+    {
+        // 왼쪽 화살표를 눌렀을 때
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-1, 0, 0);
+        }
+
+        // 오른쪽 화살표를 눌렀을 때
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(1, 0, 0);
+        }
+    }
+
+    public void LeftMove()
+    {
+        transform.Translate(-1, 0, 0);
+    }
+
+    public void RightMove()
+    {
+        transform.Translate(1, 0, 0);
+    }
+}
